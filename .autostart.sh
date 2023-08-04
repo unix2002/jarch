0a3b3ea26201433cc1fae6857cf9407e5321f898
Joseph Krol <josephkrol2002@gmail.com>
2023-08-04 13:55:26 +0200

    Added better comments to my autostart script.

--- .autostart.sh
+++ .autostart.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+# This program is run everytime xmonad is run.
+# It is located at /home/<usr>/.autostart.sh
 
-# Check if the script is already running
-# if pgrep -x "loop_pngs.sh" >/dev/null; then
-#     echo " "
-# else
-#     # Start the script
-#     /home/joseph/.background/loop_pngs.sh /home/joseph/.background/bg &
-#     disown
-# fi
-
+# These commands maps my escape key to caps-lock and vise versa.
 xbindkeys &
 xmodmap /home/joseph/.Xmodmap &
+
+# These commands set a gif as my background using xwinwrap.
 sleep 2
 xwinwrap -b -sp -fs -nf -ov -- gifview -w WID /home/joseph/.background/gifs/fountain_pen_vibe.gif -a &
+
+# This command start my music player deamon.
 mpd &
